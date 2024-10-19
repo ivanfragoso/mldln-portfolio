4ac425251d9d1860e993ccf6be70f30fd866b72b
ifg-dev <ivanfragosogar@outlook.es>
2024-10-19 17:34:22 +0200

    feat(fonts): upload and set project fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,15 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const PPEditorialOld = localFont({
+  src: "./fonts/PPEditorialOld-Italic.woff",
+  variable: "--font-pp-editorial-old",
+  weight: "400",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+
+const BlurWeb = localFont({
+  src: "./fonts/BlurWeb-Medium.otf",
+  weight: "500",
 });
 
 export const metadata: Metadata = {
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${PPEditorialOld.variable} ${BlurWeb.className} antialiased`}
       >
         {children}
       </body>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,10 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        editorial: ['var(--font-pp-editorial-old)'],
+        blurweb: ['var(--font-blur-web)'],
+      },
     },
   },
   plugins: [],
