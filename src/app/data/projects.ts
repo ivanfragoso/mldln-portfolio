40b758cf1d6d5d2052e4989a66476660001d52bb
ifg-dev <ivanfragosogar@outlook.es>
2024-10-19 17:36:28 +0200

    feat: set array of json projects

---
+++ src/app/data/projects.ts
@@ -0,0 +1,87 @@
+import rogeImage from '@/app/images/roge.jpeg';
+import leSilleImage from '@/app/images/le_sille.jpeg';
+import gorgiImage from '@/app/images/gorgi.jpeg';
+import la111Image from '@/app/images/la111.jpeg';
+import lewisImage from '@/app/images/lewis.jpeg';
+import aetheriaImage from '@/app/images/aetheria.jpeg';
+
+export const projects = [
+    {
+        number: "01",
+        title: "Roge's Bakery",
+        image: rogeImage,
+        firstLineTags: [
+            { text: "Brand Identity - Branding", margin: "m-0" },
+            { text: "Posters", margin: "m-0" }
+        ],
+        secondLineTags: [
+            { text: "Art Direction", margin: "m-0" },
+            { text: "Methodology", margin: "m-0" }
+        ]
+    },
+    {
+        number: "02",
+        title: "LeSille",
+        image: leSilleImage,
+        firstLineTags: [
+            { text: "Card Game - Educative", margin: "m-0" },
+            { text: "Illustration", margin: "m-0" }
+        ],
+        secondLineTags: [
+            { text: "Packaging", margin: "m-0" },
+            { text: "History of Interiorism", margin: "m-0" }
+        ]
+    },
+    {
+        number: "03",
+        title: "Gorgi",
+        image: gorgiImage,
+        firstLineTags: [
+            { text: "Wine Label", margin: "m-0" },
+            { text: "Illustration", margin: "m-0" }
+        ],
+        secondLineTags: [
+            { text: "Art Direction", margin: "m-0" },
+            { text: "Brand Identity", margin: "m-0" }
+        ]
+    },
+    {
+        number: "04",
+        title: "La 111",
+        image: la111Image,
+        firstLineTags: [
+            { text: "Wine Label", margin: "m-0" },
+            { text: "Illustration", margin: "m-0" }
+        ],
+        secondLineTags: [
+            { text: "Art Direction", margin: "m-0" },
+            { text: "Brand Identity", margin: "m-0" }
+        ]
+    },
+    {
+        number: "05",
+        title: "Lewis",
+        image: lewisImage,
+        firstLineTags: [
+            { text: "Wine Label", margin: "m-0" },
+            { text: "Illustration", margin: "m-0" }
+        ],
+        secondLineTags: [
+            { text: "Art Direction", margin: "m-0" },
+            { text: "Brand Identity", margin: "m-0" }
+        ]
+    },
+    {
+        number: "06",
+        title: "Aetheria",
+        image: aetheriaImage,
+        firstLineTags: [
+            { text: "Wine Label", margin: "m-0" },
+            { text: "Illustration", margin: "m-0" }
+        ],
+        secondLineTags: [
+            { text: "Art Direction", margin: "m-0" },
+            { text: "Brand Identity", margin: "m-0" }
+        ]
+    }
+]
