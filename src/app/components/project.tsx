8a2aaba398961efeaf7280d8577f2ce50b4810d6
ifg-dev <ivanfragosogar@outlook.es>
2024-10-19 20:29:22 +0200

    feat(project): show "view project" button on image hover

--- src/app/components/project.tsx
+++ src/app/components/project.tsx
@@ -21,14 +21,19 @@ export default function Project({
   secondLineTags,
 }: ProjectProps) {
   return (
-    <div className="flex flex-col items-center gap-9 cursor-pointer">
+    <div className="flex flex-col items-center gap-9">
       <div className="text-center relative">
         <h2 className="text-5xl font-editorial">({number})</h2>
         <h2 className="text-5xl text-primary font-bold uppercase -mt-5">
           {title}
         </h2>
       </div>
-      <Image src={image} alt={title} priority />
+      <div className="relative cursor-pointer">
+        <div className="absolute flex items-center justify-center w-full h-full bg-secondary/30 opacity-0 hover:opacity-100">
+          <span className="text-base text-white uppercase hover:text-primary">( View project )</span>
+        </div>
+        <Image src={image} alt={title} priority />
+      </div>
       <div className="w-full text-lg">
         <div className="flex justify-between">
           {firstLineTags.map((tag, index) => (
