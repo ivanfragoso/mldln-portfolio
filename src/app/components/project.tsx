5f9ceac548fe993b26b1394b9701c0216720b5c7
ifg-dev <ivanfragosogar@outlook.es>
2024-10-27 02:09:37 +0100

    feat(Project): set image width and set imageClick trigger

--- src/app/components/project.tsx
+++ src/app/components/project.tsx
@@ -11,6 +11,7 @@ interface ProjectProps {
   image: StaticImageData;
   firstLineTags: TagProps[];
   secondLineTags: TagProps[];
+  imageClick: () => void;
 }
 
 export default function Project({
@@ -19,20 +20,21 @@ export default function Project({
   image,
   firstLineTags,
   secondLineTags,
+  imageClick
 }: ProjectProps) {
   return (
-    <div className="flex flex-col items-center gap-9">
+    <div className="flex flex-col items-center gap-[16px]">
       <div className="text-center relative">
         <h2 className="text-5xl font-editorial">({number})</h2>
-        <h2 className="text-5xl text-primary font-bold uppercase -mt-5">
+        <h2 className="text-5xl text-primary font-bold uppercase -mt-4">
           {title}
         </h2>
       </div>
       <div className="relative cursor-pointer">
-        <div className="absolute flex items-center justify-center w-full h-full bg-secondary/30 opacity-0 hover:opacity-100">
-          <span className="text-base text-white uppercase hover:text-primary">( View project )</span>
+        <div className="absolute flex items-center justify-center w-full h-full bg-secondary/20 opacity-0 hover:opacity-100">
+          <span className="text-base text-white uppercase" onClick={imageClick}>( View project )</span>
         </div>
-        <Image src={image} alt={title} priority />
+        <Image src={image} alt={title} width={410} priority />
       </div>
       <div className="w-full text-lg">
         <div className="flex justify-between">
@@ -53,3 +55,17 @@ export default function Project({
     </div>
   );
 }
+
+
+
+
+
+    /*}
+    <div
+      className={`absolute top-0 w-full h-full bg-white z-10 ${
+        animateBg ? "bg-animate-in" : "bg-animate-out"
+      }`}
+    >
+      <h3 className="float-right mr-6 text-lg font-editorial mt-5 cursor-pointer" onClick={handleClick}>Back</h3>
+    </div>
+    */
