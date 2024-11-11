659f0987b991952a6d15722fa92e93dc9d39c7dc
ifg-dev <ivanfragosogar@outlook.es>
2024-11-11 14:02:47 +0100

    fix(AboutDialog): change little missspelling

--- src/app/components/AboutDialog.tsx
+++ src/app/components/AboutDialog.tsx
@@ -14,10 +14,10 @@ export default function AboutDialog({ showAbout, onClickOutside }: AboutDialogPr
       <div className={`absolute ${showAbout ? "bounce-up" : "bounce-down"} z-40 left-0 right-0 mx-auto inset-0 w-[866px] bg-background h-[380px] p-11`}>
         <h3 className="uppercase text-primary text-[64px] font-blurweb leading-[3rem] mb-5">Hi!</h3>
         <p className="mb-4">
-          Soy <span className="text-primary">Marta López de la Nieta</span>, diseño gráfica establecida en Barcelona!
+          Soy <span className="text-primary">Marta López de la Nieta</span>, diseñadora gráfica establecida en Barcelona!
         </p>
         <p className="mb-4">
-          A lo largo de mi formación como diseño he tenido la oportunidad de experimentar con varias <br />
+          A lo largo de mi formación como diseñadora he tenido la oportunidad de experimentar con varias <br />
           disciplinas dentro del mundo del diseño gráfico, con proyectos que iban desde la <span className="text-primary">identidad de marca, <br />
           como la fabricación digital realizando maquetas o proyectos audiovisuales.</span>
         </p>
